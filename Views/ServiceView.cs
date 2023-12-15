d0bb50340204c0d6e0bc8154e5b70893670fd01d
Fabio Weck <fabio.weck@gmail.com>
2023-12-14 20:31:09 -0700

    comments added

--- Views/ServiceView.cs
+++ Views/ServiceView.cs
@@ -10,13 +10,16 @@ namespace BusinessManager
 {
     public partial class MainForm
     {
+        //Method to filter table by service
         private void btnFindService_Click(object sender, EventArgs e)
         {
         
+            //If filter by ID is checked, then proceed
             if (radBtnServiceID.Checked)
             {
                 foreach (DataGridViewRow row in servicesDataGrid.Rows)
                 {
+                    //If finds the row, the highlight the item on the table
                     if (row.Cells[0].Value != null && row.Cells[0].Value.ToString() == txtSearchService.Text)
                     {
                         servicesDataGrid.ClearSelection();
@@ -26,10 +29,12 @@ namespace BusinessManager
                 }
             }
 
+            //If filter by description is checked, then proceed
             if (radBtnDescription.Checked)
             {
                 foreach (DataGridViewRow row in servicesDataGrid.Rows)
                 {
+                    //If finds the row, the highlight the item on the table
                     if (row.Cells[1].Value != null && row.Cells[1].Value.ToString().ToLower().Contains(txtSearchService.Text.ToLower()))
                     {
                         servicesDataGrid.ClearSelection();
@@ -40,27 +45,36 @@ namespace BusinessManager
             }
         }
 
+        //Method to open the item profile view
         private void btn_itemDetail_Click(object sender, EventArgs e)
         {
             if (servicesDataGrid.SelectedRows.Count > 0)
             {
+                //Get the row data
                 DataGridViewRow selectedRow = servicesDataGrid.SelectedRows[0];
 
+                //Get the information
                 int id = (int)selectedRow.Cells["Id"].Value;
                 string description = selectedRow.Cells["Description"].Value.ToString();
                 string price = selectedRow.Cells["Price"].Value.ToString();
 
+                //Open a new item profile view with data
                 ItemProfileView itemProfileView = new ItemProfileView(id, description, double.Parse(price), isAdmin);
                 itemProfileView.ShowDialog();
 
+                //Refresh services data grid view
                 servicesDataGrid.Refresh();
             }
         }
 
+        //Method to add an item to the table
         private void btn_addItem_Click(object sender, EventArgs e)
         {
+            //Open a new Item profile view
             ItemProfileView addItem = new ItemProfileView();
             addItem.ShowDialog();
+
+            //Refresh services data grid view
             servicesDataGrid.Refresh();
         }
     }
